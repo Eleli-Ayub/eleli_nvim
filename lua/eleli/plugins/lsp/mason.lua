6549e85c24b3dbad13f76a48f0727b38457369b5
Eleli Ayub <eleliayub@gmail.com>
2026-05-20 06:40:41 +0300

    removed comment string for crashes when type returns null

--- lua/eleli/plugins/lsp/mason.lua
+++ lua/eleli/plugins/lsp/mason.lua
@@ -1,36 +1,34 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	opts = {
-    ensure_installed = {
-      "goimports",
-      "gofumpt", 
-      "gomodifytags", 
-      "gopls",
-      "impl", 
-      "delve", 
-      "golangci-lint" 
-    }
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		local mason_lspconfig = require("mason-lspconfig")
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		local mason_tool_installer = require("mason-tool-installer")
-
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "gopls",
+        "impl",
+        "delve",
+        "golangci-lint",
+      },
+    })
+  end,
 }
